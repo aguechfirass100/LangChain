704d333efc8c9c69b5c005bd5907d57ffe9e200f
aguechfirass100 <aguechfirass100@gmail.com>
2025-06-23 15:29:29 +0100

    feat: integrate Ollama gemma3:1b model in Streamlit app

--- chatbot/local_llama.py
+++ chatbot/local_llama.py
@@ -2,20 +2,12 @@ import os
 
 import streamlit as st
 from dotenv import load_dotenv
+from langchain_community.llms import Ollama
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_openai import ChatOpenAI
 
-# import google.generativeai as genai
-# from langchain_google_genai import ChatGoogleGenerativeAI
-
 load_dotenv()
-
-os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
-# os.environ["GEMINI_API_KEY"] = os.getenv("GEMINI_API_KEY")
-# LangSmith tracking
-os.environ["LANGCHAIN_TRACING_V2"] = "true"
-os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
 
 # Prompt Template
 prompt = ChatPromptTemplate.from_messages(
@@ -26,16 +18,13 @@ prompt = ChatPromptTemplate.from_messages(
 )
 
 # Streamlit Framework
-st.title("Langchain Demo with OpenAI API")
+st.title("Langchain Demo with Ollama - Local LLMs - gemma3:1b")
 input_text = st.text_input("Search the topic you want")
 
-# LLMs
-llm_openai = ChatOpenAI(model="gemini-1.5-turbo")
-# llm_gemini = ChatGoogleGenerativeAI(model="gemini-1.5-flash")
-# model = genai.GenerativeModel("gemini-1.5-flash")
-
+# Ollama LLM
+llm = Ollama(model="gemma3:1b")
 output_parser = StrOutputParser()
-chain = prompt | llm_openai | output_parser
+chain = prompt | llm | output_parser
 
 if input_text:
     st.write(chain.invoke({"question": input_text}))
